69aad9b1efdf847487fd5307a474f9040926cd55
Tahmid Shakil <at.shakil.92@gmail.com>
2017-04-15 02:18:49 +0600

    i18n Support added

--- app/components/Primary.js
+++ app/components/Primary.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import I18n from 'react-native-i18n';
 
 export default class Primary extends Component {
   render() {
     return (
-      <Text>Hey</Text>
+      <Text>{I18n.t('greetings')}</Text>
     );
   }
 }
--- app/containers/App.js
+++ app/containers/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import { Provider, connect } from 'react-redux';
 import { addNavigationHelpers } from 'react-navigation';
+import i18n from 'react-native-i18n';
 import AppNavigator from '../navigators/App';
 import store from '../store';
+import locales from '../locales';
+
+i18n.fallbacks = true;
+i18n.translations = locales;
+// Overrides device locale. Remove when necessary locales are available
+i18n.locale = "en";
 
 @connect(
   state => ({
