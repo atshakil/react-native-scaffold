939ee28e11cd96f4afb0a32a4a4ef40b7ccb6618
Tahmid Shakil <at.shakil.92@gmail.com>
2017-04-16 05:34:31 +0600

    Push notification support added

--- android/app/src/main/java/com/react_native_scaffold/MainApplication.java
+++ android/app/src/main/java/com/react_native_scaffold/MainApplication.java
@@ -3,6 +3,7 @@ package com.react_native_scaffold;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new VectorIconsPackage(),
             new ReactNativeI18n()
       );
--- app/containers/App.js
+++ app/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { Provider, connect } from 'react-redux';
 import { addNavigationHelpers } from 'react-navigation';
 import i18n from 'react-native-i18n';
+import PushNotification from 'react-native-push-notification';
 import AppNavigator from '../navigators/App';
 import store from '../store';
 import locales from '../locales';
@@ -14,10 +15,24 @@ i18n.locale = "en";
 @connect(
   state => ({
     nav: state.nav
+  }),
+  dispatch => ({
+    initiatePushNotification() {
+      PushNotification.configure({
+        onNotification(notification) {
+          // TODO: Implement notification handler service
+          console.warn(JSON.stringify(notification));
+        }
+      });
+    }
   })
 )
 
 class App extends Component {
+  componentWillMount() {
+    this.props.initiatePushNotification();
+  }
+
   render() {
     const { nav, dispatch } = this.props;
 
