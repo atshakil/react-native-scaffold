702aa7809d08b6573b63c4014a8a704c81805082
Tahmid Shakil <at.shakil.92@gmail.com>
2017-07-29 01:19:38 +0600

    Basic FCM support

--- android/app/src/main/java/com/react_native_scaffold/MainApplication.java
+++ android/app/src/main/java/com/react_native_scaffold/MainApplication.java
@@ -3,6 +3,7 @@ package com.react_native_scaffold;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.evollu.react.fcm.FIRMessagingPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FIRMessagingPackage(),
             new VectorIconsPackage(),
             new ReactNativeI18n()
       );
--- app/containers/App.js
+++ app/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { Provider, connect } from 'react-redux';
 import { addNavigationHelpers } from 'react-navigation';
 import i18n from 'react-native-i18n';
+import FCM, { FCMEvent, FCMEventType } from 'react-native-fcm';
 import AppNavigator from '../navigators/App';
 import store from '../store';
 import locales from '../locales';
@@ -21,6 +22,26 @@ i18n.locale = "en";
 )
 
 class App extends Component {
+  componentDidMount() {
+    console.warn("Check log for FCM tokens");
+    FCM.requestPermissions();
+    FCM.getFCMToken().then(token => {
+      // Send token to server
+      console.log(`Token: ${token}`);
+    });
+    // Notification lister can be added here
+    this.refreshTokenListener = FCM.on(FCMEvent.RefreshToken, token => {
+      // Send token to server
+      console.log(`Refresh token: ${token}`);
+    });
+  }
+
+  componentWillUnmount() {
+    console.log("Unmounting app...");
+    // If a notification lister is added, it can be removed here
+    this.refreshTokenListener.remove();
+  }
+
   render() {
     const { nav, dispatch } = this.props;
 
